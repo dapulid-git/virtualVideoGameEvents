606cc8e4c0238deac0d6bd0097db5ca36cc83508
dapulid-git <dpulido022@gmail.com>
2024-06-25 02:24:08 -0500

    The lambda create user function is modified to create a connection to the database in dynamodb

--- src/handlers/user/readUser.mjs
+++ src/handlers/user/readUser.mjs
@@ -1,11 +1,59 @@
-export const readUserLambdaHandler  = async (event) => {
-  console.log(event);
-  const response = {
-    statusCode: 201,
-    body: JSON.stringify({
-      detail: `holaaa`
-    })
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocument.from(client);
+
+const tableName = process.env.USER_TABLE;
+
+export const putItemHandler = async (event) => {
+
+  const { userId, userName, userType } = JSON.parse(event.body);
+
+  var response = {};
+
+  var readParams = {
+    TableName: tableName,
+    Key: { userId: userId }
   };
 
+  var createParams = {
+    TableName: tableName,
+    Item: {
+      userId: userId,
+      userName: userName,
+      userType: userType
+    }
+  };
+
+  try {
+    const readData = await ddbDocClient.get(readParams);
+
+    if (readData.Item) {
+      if (readData.Item.userId === userId) {
+        response = {
+          statusCode: 400,
+          body: JSON.stringify({
+            title: "Bad Request",
+            code: 400,
+            detail: `User with id: ${userId} is already registered.`
+          })
+        };
+      }
+    } else {
+      await ddbDocClient.put(createParams);
+
+      response = {
+        statusCode: 201,
+        body: JSON.stringify({
+          detail: `User: ${userName} with id: ${userId} was created successfully`
+        })
+      };
+    }
+
+  } catch (err) {
+    throw new Error(err.stack);
+  }
+
   return response;
-}
+};
